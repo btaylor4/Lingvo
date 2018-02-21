805d80b443adf29925e1ae2a3793d552d5f7f9fb
rolstenhouse <rolsthoorn12@gmail.com>
2018-02-21 11:01:25 -0500

    Basic speech to text from client

--- app/static/js/video.jsx
+++ app/static/js/video.jsx
@@ -10,11 +10,49 @@ var mediaRecorder;
 var socket = io.connect('localhost:5000/test');
     
 export default class StartVideo extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      interim_text : "",
+      final_text : "",
+      recognition : {}
+    }
+    this.enableTranslation = this.enableTranslation.bind(this);
+  }
+
+  componentDidMount() {
+    if (!('webkitSpeechRecognition' in window)) {
+      console.log('Upgrade browser');
+    } else {
+      var recognition = new webkitSpeechRecognition();
+      recognition.continuous = true;
+      recognition.interimResults = true;
+      recognition.lang = 'en-US';
+
+      recognition.onresult = event => {
+        for (var i = event.resultIndex; i < event.results.length; ++i) {
+          if (event.results[i].isFinal) {
+            var finalText = this.state.final_text;
+            this.setState({final_text: finalText + event.results[i][0].transcript});
+          } else {
+            var interimText = this.state.interim_text;
+            this.setState({interim_text: interimText + event.results[i][0].transcript});
+          }
+        }
+      }
+
+      this.setState({recognition: recognition});
+    }
+  }
+
   render () {
     return <div>
       <button type="button" onClick={this.startVideo}>Start</button>
     <button type="button" onClick={this.enableTranslation}>Enable Translation</button>
     <button type="button" onClick={this.disableTranslation}>Disable Translation</button>
+    <hr></hr>
+    <p>Interim text: {this.state.interim_text}</p>
+    <p>Final text: {this.state.final_text}</p>
     </div>
   }
   
@@ -40,23 +78,10 @@ export default class StartVideo extends React.Component {
   }
 
   enableTranslation() {
-    // Get local audio stream
-    var audioStream = new MediaStream(localStream.getAudioTracks());
-
-    // Set recorder on audiostream
-    mediaRecorder = new MediaRecorder(audioStream);
-
-    // Chunk into 100 ms blobs
-    mediaRecorder.start(100);
-
-    mediaRecorder.ondataavailable = function(e) {
-      console.log(e);
-      // Sending over just the blob
-      socket.emit('audio', {data:e.data})
-    }
+    this.state.recognition.start();
   }
-
+  
   disableTranslation() {
-    mediaRecorder.stop();
+    recognition = this.state.recognition.stop();
   }
 }
