cb7379e7216aa79840c6532f9d762e7b0774d0c7
Bryan Taylor <clearly.b.t@gmail.com>
2018-02-23 22:30:36 -0500

    Deleting commented out code

--- app/static/js/index.jsx
+++ app/static/js/index.jsx
@@ -1,9 +1,4 @@
 // index.jsx
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import ShowVideo from "./video"
 import Search from "./client"
-// ReactDOM.render(<ShowVideo />, document.getElementById("sourceVideoContent"));
-// ReactDOM.render(<ConnectButton />, document.getElementById("remoteVideoContent"));
-// ReactDOM.render(<Search />, document.getElementById("searchbar"));
