e8a3c781fbe38c48df841630e1469536aed55058
rolstenhouse <rolsthoorn12@gmail.com>
2018-04-06 11:30:21 -0400

    removing second return

--- app/static/js/nav.jsx
+++ app/static/js/nav.jsx
@@ -36,37 +36,5 @@ export default class NavBar extends React.Component {
         </div>
       </nav>
     );
-
-    return (
-      <nav className="navbar navbar-inverse bg-inverse navbar-expand-lg">
-        <div className="navbar-header">
-          <a className="navbar-brand" href="#">
-            Lingvo
-          </a>
-        </div>
-        <div className="navbar-collapse collapse">
-          <ul className="nav navbar-nav">
-            <li>
-              <a href="#">About</a>
-            </li>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-          </ul>
-          <ul className="nav navbar-nav navbar-right">
-            <li>
-              <a href="/login">
-                <span className="glyphicon glyphicon-log-in" /> Login
-              </a>
-            </li>
-            <li>
-              <a href="/register">
-                <span className="glyphicon glyphicon-user" /> Sign Up
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    );
   }
 }
